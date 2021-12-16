cdcc031b97cb538c9ee4e28bfe6c5636aebdfbaa
Cahyadi Krishna <65574827+cahyadikrishna@users.noreply.github.com>
2021-12-16 23:53:29 +0800

    Add files via upload
    first commit

---
+++ src/com/mycompany/pbogui/userData.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbogui;
+
+/**
+ *
+ * @author adipr
+ */
+public interface userData {
+    public String username = "admin";
+    public String password = "primakara";
+}
